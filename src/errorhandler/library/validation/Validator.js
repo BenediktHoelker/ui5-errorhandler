9eb727f877017d5a7fbb54264d849cf8b95731e6
Benedikt Hölker <benedikt.hoelker@gmail.com>
2020-05-19 10:02:55 +0200

    Refactor Validator.js

--- src/errorhandler/library/validation/Validator.js
+++ src/errorhandler/library/validation/Validator.js
@@ -1,19 +1,51 @@
 sap.ui.define(["sap/ui/core/ValueState"], function (ValueState) {
-  /**
-   * Check if the control is required
-   */
+  function getRequired(control) {
+    return control.getRequired && control.getRequired();
+  }
+
+  function getEditable(control) {
+    return control.getProperty && control.getProperty("editable");
+  }
+
+  function getEnabled(control) {
+    return control.getEnabled && control.getEnabled();
+  }
+
+  function getVisible(control) {
+    return control.getVisible && control.getVisible();
+  }
+
+  function getHasInnerControls(control) {
+    return control.getInnerControls && control.getInnerControls().length > 0;
+  }
+
+  function getValidationContext(control, prop) {
+    return {
+      control,
+      binding: control.getBinding(prop),
+      externalValue: control.getProperty(prop),
+      prop,
+      valueState: control.getValueState(),
+      valueStateText: control.getValueStateText(),
+    };
+  }
+
   function validateRequired({ control, propsToValidate }) {
+    if (!getRequired(control)) {
+      return [];
+    }
+
     return propsToValidate
-      .filter(() => control.getRequired && control.getRequired())
       .filter((prop) => control.getBinding(prop))
-      .map((prop) => {
-        const externalValue = control.getProperty(prop);
+      .map((prop) => getValidationContext(control, prop))
+      .map((vldContext) => {
+        const { externalValue } = vldContext;
 
         if (!externalValue || externalValue === "") {
           return {
-            control,
+            ...vldContext,
             valueState: ValueState.Error,
-            message: "Please fill this mandatory field!",
+            valueStateText: "The field is required but empty => error",
           };
         }
         if (
@@ -22,26 +54,30 @@ sap.ui.define(["sap/ui/core/ValueState"], function (ValueState) {
         ) {
           // might be a select
           return {
-            control,
+            ...vldContext,
             valueState: ValueState.Error,
             message: "Please choose an entry!",
           };
         }
         return {
-          control,
+          ...vldContext,
           valueState: ValueState.None,
-          message: "All good",
+          valueStateText: "The field is required and filled => good",
         };
       });
   }
 
   function validateConstraints({ control, propsToValidate }) {
+    if (!getEnabled(control) || !getEditable(control)) {
+      return [];
+    }
+
     return propsToValidate
       .filter((prop) => control.getBinding(prop))
-      .map((prop) => {
+      .map((prop) => getValidationContext(control, prop))
+      .map((vldContext) => {
         try {
-          const binding = control.getBinding(prop);
-          const externalValue = control.getProperty(prop);
+          const { binding, externalValue } = vldContext;
           const internalValue = binding
             .getType()
             .parseValue(externalValue, binding.sInternalType);
@@ -49,13 +85,12 @@ sap.ui.define(["sap/ui/core/ValueState"], function (ValueState) {
           binding.getType().validateValue(internalValue);
 
           return {
-            control,
-            valueState: ValueState.None,
-            message: "All good",
+            ...vldContext,
+            message: "All field-constraints are met => good",
           };
         } catch (ex) {
           return {
-            control,
+            ...vldContext,
             valueState: ValueState.Error,
             message: ex.message,
           };
@@ -63,17 +98,23 @@ sap.ui.define(["sap/ui/core/ValueState"], function (ValueState) {
       });
   }
 
-  function validateCustom({ control }) {
-    return {
-      control,
-      valueState: control.getValueState(),
-      message: control.getValueStateText(),
-    };
+  function updateUI(validations) {
+    const validationsByControl = new Map(
+      validations
+        .sort((a, b) => a.valueState.localeCompare(b.valueState))
+        .reverse()
+        .map((validation) => [validation.control.getId(), validation])
+    );
+
+    validationsByControl.forEach(({ control, valueState, valueStateText }) => {
+      control.setValueState(valueState);
+      control.setValueStateText(valueStateText);
+    });
   }
 
   class Validator {
     constructor() {
-      this.aggregationNames = [
+      this.possibleAggregations = [
         "items",
         "content",
         "form",
@@ -86,89 +127,62 @@ sap.ui.define(["sap/ui/core/ValueState"], function (ValueState) {
         "cells",
         "page",
       ];
-      this.validateProperties = ["value", "selectedKey", "text"]; // yes, I want to validate Select and Text controls too
+      this.propsToValidate = ["value", "selectedKey", "text"]; // yes, I want to validate Select and Text controls too
     }
 
     /**
      * Recursively validates the given control and any aggregations (i.e. child controls) it may have
      */
     validate(parent) {
-      sap.ui.getCore().getMessageManager().removeAllMessages();
+      const validations = this.getValidations([], parent);
 
-      const invalid = this.getValidations([], parent).find(
+      updateUI(validations);
+
+      return validations.some(
         ({ valueState }) => valueState === ValueState.Error
       );
-
-      if (invalid) {
-        // const { control, valueState, message } = invalid;
-
-        // control.setValueState(valueState);
-        // control.setValueStateText(message);
-
-        return false;
-      }
-
-      return true;
     }
 
     /**
      * Recursively validates the given control and any aggregations (i.e. child controls) it may have
      */
-    getValidations(acc, control) {
+    getValidations(
+      acc,
+      control,
+      propsToValidate = this.propsToValidate,
+      possibleAggregations = this.possibleAggregations
+    ) {
       // only validate controls and elements which have a 'visible' property
       // and are visible controls (invisible controls make no sense checking)
-      if (
-        !(
-          (control instanceof sap.ui.core.Control ||
-            control instanceof sap.ui.layout.form.FormContainer ||
-            control instanceof sap.ui.layout.form.FormElement ||
-            control instanceof sap.m.IconTabFilter) &&
-          control.getVisible()
-        )
-      ) {
+      if (!getVisible(control)) {
         return acc;
       }
 
-      if (
-        typeof control.getInnerControls === "function" &&
-        control.getInnerControls().length > 0
-      ) {
+      if (getHasInnerControls(control)) {
         return this.getValidations(acc, control.getInnerControls()[0]);
       }
 
-      if (!(typeof control.getValueState === "function")) {
+      if (!control.getValueState) {
         // no validation possible => yet check for aggregations
 
-        return this.aggregationNames
+        return possibleAggregations
           .flatMap((name) => control.getAggregation(name))
-          .filter(
-            (ctrl) =>
-              ctrl &&
-              ctrl.getEnabled &&
-              ctrl.getEnabled() &&
-              ctrl.getProperty &&
-              ctrl.getProperty("editable")
-          )
           .filter(Boolean)
           .reduce((arr, ctrl) => this.getValidations(arr, ctrl), acc);
       }
 
-      return acc.concat(
-        [
+      return acc
+        .concat(
           validateRequired({
             control,
-            propsToValidate: this.validateProperties,
+            propsToValidate,
           }),
           validateConstraints({
             control,
-            propsToValidate: this.validateProperties,
-          }),
-          validateCustom({
-            control,
-            propsToValidate: this.validateProperties,
-          }),
-        ].flat()
-      );
+            propsToValidate,
+          })
+        )
+        .flat();
     }
   }
 
