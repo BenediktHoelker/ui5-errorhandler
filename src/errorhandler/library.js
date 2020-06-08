f051a6d86d6a11265e39088d02653545f2eb1367
Adriana Rieber <--adriana.rieber@iot-online.de>
2020-06-08 18:28:34 +0200

    Use a seperated file for the service-functions which should not be called from the apps

--- src/errorhandler/library.js
+++ src/errorhandler/library.js
@@ -2,10 +2,10 @@ sap.ui.define(
   [
     "sap/ui/core/message/Message",
     "./handling/MessagePopover",
-    "./handling/ODataErrorHandling",
     "sap/ui/model/resource/ResourceModel",
+    "./Service",
   ],
-  function (Message, MessagePopover, ODataErrorHandling, ResourceModel) {
+  function (Message, MessagePopover, ResourceModel, Service) {
     sap.ui.getCore().initLibrary({
       name: "errorhandler",
       version: "1.0.0",
@@ -28,40 +28,44 @@ sap.ui.define(
           bundleName: "errorhandler.i18n.i18n",
         }).getResourceBundle();
 
-        this.ODataErrorHandling = new ODataErrorHandling({
-          resBundle: this.resBundle,
-          messageModel: this.getMessageModel(),
-        });
-
         this.messagePopover = new MessagePopover({
           resBundle: this.resBundle,
           messageModel: this.getMessageModel(),
         });
 
+        this.service = new Service({
+          resBundle: this.resBundle,
+          messageManager: this.getMessageManager(),
+          messageModel: this.getMessageModel(),
+        });
+
         this.backendMessages = [];
+
+        this.msgProcessor = new sap.ui.core.message.ControlMessageProcessor();
+        this.getMessageManager().registerMessageProcessor(this.msgProcessor);
 
         ODataModels.forEach((model) => {
           Promise.all([
-            this.waitForAppToBeRendered(viewModel),
-            this.onMetadataFailed(model),
+            this.service.waitForAppToBeRendered(viewModel),
+            this.service.onMetadataFailed(model),
           ]).then(() => {
             viewModel.setProperty("/busy", false);
 
-            this.showError({
+            this.service.showError({
               blocking: true,
               error: this.resBundle.getText("metadataLoadingFailed"),
             });
           });
 
           model.attachMessageChange((oEvent) => {
-            this.backendMessages = this.getNewBckndMsgs(oEvent);
+            this.backendMessages = this.service.getNewBckndMsgs(oEvent);
 
             const error = this.backendMessages.find(
               (message) => message.getType() === sap.ui.core.MessageType.Error
             );
 
             if (error) {
-              this.showError({
+              this.service.showError({
                 error,
               });
             }
@@ -70,82 +74,10 @@ sap.ui.define(
           model.attachRequestFailed((event) => {
             // falls der Request fehlschlägt, jedoch keine Message geliefert wurde trat ein Timeout bzw. Verbindungsabbruch auf
             if (this.backendMessages.length === 0) {
-              this.showConnectionError(event);
+              this.service.showConnectionError(event);
             }
           });
         });
-      },
-
-      waitForAppToBeRendered(viewModel) {
-        if (viewModel.getProperty("/isRendered")) {
-          return Promise.resolve();
-        }
-
-        return new Promise((resolve) =>
-          viewModel.attachPropertyChange(() => {
-            if (viewModel.getProperty("/isRendered")) {
-              resolve();
-            }
-          })
-        );
-      },
-
-      onMetadataFailed(ODataModel) {
-        if (ODataModel.isMetadataLoadingFailed()) {
-          return Promise.resolve();
-        }
-
-        return new Promise((resolve) =>
-          ODataModel.attachMetadataFailed(() => resolve())
-        );
-      },
-
-      getNewBckndMsgs(event) {
-        const newMessages = event.getParameter("newMessages") || [];
-        const uniqueMessages = this.getUnique(newMessages);
-
-        const duplicates = newMessages.filter(
-          (msg) => !uniqueMessages.includes(msg)
-        );
-
-        this.getMessageManager().removeMessages(duplicates);
-
-        return uniqueMessages;
-      },
-
-      getUnique(messages) {
-        const messagesMap = {};
-
-        messages.forEach((msg) => {
-          const key = msg.message ? msg.message.toString() : "01";
-
-          messagesMap[key] = msg;
-        });
-
-        return Object.values(messagesMap);
-      },
-
-      showError(params) {
-        this.ODataErrorHandling.showError(params);
-      },
-
-      showConnectionError(event) {
-        const response = event.getParameter("response");
-        const { responseText, statusCode } = response;
-
-        if (responseText.includes("Timed Out") || statusCode === 504) {
-          this.showError({
-            error: this.resBundle.getText("timedOut"),
-          });
-        }
-      },
-
-      getMsgProcessor() {
-        if (!this.msgProcessor) {
-          this.msgProcessor = new sap.ui.core.message.ControlMessageProcessor();
-          this.getMessageManager().registerMessageProcessor(this.msgProcessor);
-        }
-        return this.msgProcessor;
       },
 
       removeMessages({ target }) {
@@ -157,32 +89,17 @@ sap.ui.define(
         this.getMessageManager().removeMessages(messages);
       },
 
-      getBindingName(input) {
-        return ["value", "selected", "selectedKey", "dateValue"].find((name) =>
-          input.getBinding(name)
-        );
-      },
-
-      getValMsgTarget(input) {
-        const isSmartField =
-          input.getMetadata().getElementName() ===
-          "sap.ui.comp.smartfield.SmartField";
-        return isSmartField
-          ? `${input.getId()}-input/value`
-          : `${input.getId()}/${this.getBindingName(input)}`;
-      },
-
       addMessage({
         text,
         input,
         message = text,
-        processor = this.getMsgProcessor(),
-        target = this.getValMsgTarget(input),
+        processor = this.msgProcessor,
+        target = this.service.getValMsgTarget(input),
         additionalText,
         type = sap.ui.core.MessageType.Error,
       }) {
         // Wenn das Binding auf das sich die Message bezieht keinen Typen besitzt, dann wird die Message bei Änderung des Binding-Wertes nicht wieder automatisch entfernt
-        const binding = input.getBinding(this.getBindingName(input));
+        const binding = input.getBinding(this.service.getBindingName(input));
         if (!binding.getType()) {
           const stringType = new sap.ui.model.type.String();
           binding.setType(stringType, "string");
