1a2eda354cfc04d9bf2b2e702ba5119929ad90d7
Benedikt Hölker <benedikt.hoelker@gmail.com>
2020-09-04 09:11:24 +0200

    Make sure, custom SmartFields are also interpreted by getValMsgTarget

--- src/errorhandler/library.js
+++ src/errorhandler/library.js
@@ -167,9 +167,10 @@ sap.ui.define(
       },
 
       getValMsgTarget(input) {
-        const isSmartField =
-          input.getMetadata().getElementName() ===
-          "sap.ui.comp.smartfield.SmartField";
+        const isSmartField = input
+          .getMetadata()
+          .getElementName()
+          .includes("SmartField");
 
         return isSmartField
           ? `${input.getId()}-input/value`
