ae0cf85409f25219b579ac1743d640dd599c67a3
Benedikt Hölker <benedikt.hoelker@iot-online.de>
2022-02-20 20:34:52 +0100

    Update cloud-service version

--- scripts/update-versions.js
+++ scripts/update-versions.js
@@ -1,11 +1,17 @@
 #!/usr/bin/env node
 
 const { writeFileSync } = require("fs");
-const { version } = require("./package.json");
+const { version } = require("../package.json");
 const manifestPath = "./src/errorhandler/manifest.json";
-
+const argv = require("minimist")(process.argv?.slice(2));
 const manifest = require(manifestPath);
+
+if (argv.btpCloudService) {
+  manifest["sap.cloud"].service = argv.btpCloudService;
+}
 
 manifest["sap.app"].applicationVersion.version = version;
 
 writeFileSync(manifestPath, JSON.stringify(manifest, null, 2) + "\n");
+
+console.log(argv);
