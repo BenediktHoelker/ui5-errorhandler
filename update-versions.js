3d0cc0a1c1616a25dfa8b3ebe6de1cc3ec6bdb1c
Benedikt Hölker <benedikt.hoelker@iot-online.de>
2022-02-20 20:40:27 +0100

    Update versions and set CP-service

--- update-versions.js
+++ update-versions.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 const { writeFileSync } = require("fs");
-const { version } = require("../package.json");
+const { version } = require("./package.json");
 const manifestPath = "./src/errorhandler/manifest.json";
 const argv = require("minimist")(process.argv?.slice(2));
 const manifest = require(manifestPath);
