da21092b9bce4f28f9f4bd39e2b9e028e135bd3b
Benedikt Hölker <benedikt.hoelker@iot-online.de>
2022-11-30 09:39:06 +0100

    chore: handle unauthorized error

--- src/errorhandler/ErrorHandler.js
+++ src/errorhandler/ErrorHandler.js
@@ -53,11 +53,17 @@ sap.ui.define(
         return this.showError(new Error(this.resBundle.getText("timedOut")));
       }
 
+      if (statusCode === 401) {
+        return this.showError(
+          "Unauthorized. Please reload the page and login again."
+        );
+      }
+
       // An entity that was not found in the service is also throwing a 404 error in OData.
       // We already cover this case with a notFound target so we skip it here.
       // A request that cannot be sent to the server is a technical error that we have to handle though
       if (
-        statusCode !== "404" ||
+        statusCode !== 404 ||
         (statusCode === 404 && responseText.indexOf("Cannot POST") === 0)
       ) {
         return this.showError(responseText);
